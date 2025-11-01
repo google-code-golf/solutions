c6eb644d9ec4b72c6b597a87f226cf44daf1ab06
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2025-10-31 21:17:46 -0400

    some trivial/charbrute post comp diamonds

---
+++ post-comp-diamonds/task008.py
@@ -0,0 +1,1 @@
+p=lambda g,*n:sorted(zip(*n or p([],*g)),key=lambda r:(8in g.__iadd__(r))*3^any(r))
---
+++ post-comp-diamonds/task336.py
@@ -0,0 +1,1 @@
+p=lambda g:exec(f"g[:]={'[r.pop()or(5in{*r}-{*r[4:]})*8for r in g],'*10};"*4)or g
