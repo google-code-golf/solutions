48dcbc1b0bf45eef30107afadd0a62cbc86a6f9f
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-11-02 18:16:01 +0000

    Comments

--- solutions/task003.py
+++ solutions/task003.py
@@ -1,3 +1,6 @@
-p=lambda m:[[v*2for v in r]for r in m+m[m[0]==m[3]:][2:5]]
+p=lambda g:[[v*2for v in r]for r in g+g[g[0]==g[3]:][2:5]]
 # ----------------------------------------------------------------
 # many
+
+# Tied
+p=lambda g:eval(str(g+g[g[2]==g[5]:][2:5]).replace(*'12'))
--- solutions/task369.py
+++ solutions/task369.py
@@ -1,3 +1,10 @@
 p=lambda g,n=7:g*-n or[*map(lambda*r,b=5:[b:=a*(a>4)or[a+b//4-.25,min(a,b)][n>3]for a in r],*p(g,n-1)[::-1])]
 # ----------------------------------------------------------------
 # many
+
+# cgi
+p=lambda g,n=7:g*-n or[*map(lambda*r,b=5:[b:=a*(a>4)or[a+b//4-.25,min(a,b)][n>3]for a in r],*p(g,n-1)[::-1])]
+# jailctf
+import re;p=lambda g:exec(r"g[::-1]=zip(*eval(re.sub('(2|3), [31]|0',r'*7%3\1%5*[3\1%31]',str(g))));"*12)or g
+# oxjam
+p=lambda g,k=-11,z=1:k*g or[[P:=v&7or[z:=z*8,4-v%7,P&~5|v][k>>3]for v in[5]+r][:0:-1]for*r,in zip(*p(g,k+1))]
