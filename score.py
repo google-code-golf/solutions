5dbe8e31ad4c66c4b0fa3d83cce60ad20f6b1a6e
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-11-02 15:50:14 +0000

    e

--- score.py
+++ score.py
@@ -2,11 +2,12 @@ import os
 import sys
 from pathlib import Path
 
-def calculate_score(directory: Path):
+
+def calculate_score(directory: Path) -> int:
     total_score = 0
 
     for problem in range(1, 401):
-        problem_file = Path(directory) / f"task{problem:03d}.py"
+        problem_file = directory / f"task{problem:03d}.py"
         if problem_file.exists():
             bytes_count = os.path.getsize(problem_file)
             score = max(1, 2500 - bytes_count)
@@ -14,6 +15,6 @@ def calculate_score(directory: Path):
 
     return total_score
 
+
 if __name__ == "__main__":
-    directory = Path(sys.argv[1] if len(sys.argv) > 1 else ".")
-    print(calculate_score(directory))
+    print(calculate_score(Path(sys.argv[1])))
