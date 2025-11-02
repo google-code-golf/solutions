077079c3f069b0c54c0e25bc0298017a9ee5716a
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-11-02 18:41:09 +0000

    more

--- solutions/task243.py
+++ solutions/task243.py
@@ -1,3 +1,6 @@
 p=lambda g:exec('g[::-1]=zip(*eval(str(g).replace("1, 0","1,1")));'*80)or g
 # ----------------------------------------------------------------
 # many
+
+# jailctf: without str.replace
+p=lambda g,i=-79:g*i or p([[r.pop()or 1in r[-1:]for r in g]for*r,in g],i+1)
