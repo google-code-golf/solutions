493a5d30bd1c94d3604745f57d5eed16a69ef86a
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2025-11-03 00:32:51 -0500

    shorten task277 hardcode i never did during comp

--- solutions/task277.py
+++ solutions/task277.py
@@ -1,3 +1,7 @@
+p=lambda g,k=-15,i=1:k*g or[[c%~c&(i:=[i*32,2-c%31*3//str(g).count("8")-(c%635in{539,547}),c|i&c%~c][k//14])for c in[0]+r][:0:-1]for*r,in zip(*p(g,k+1))]
+# ----------------------------------------------------------------
+# post-competition-diamond, base: jailctf annotations
+
 z=[0];p=lambda g,k=38,h=2,q=z*9:~k*g or p([q:=[v and[v%63,P|p|v,h:=h*64,v//sum(g,z).count(v)][k>>4]for P,p,v in zip(z+q,z+r,r)]for*r,in zip(*g[::-1])],k-1)
 # ----------------------------------------------------------------
 # oxjam
