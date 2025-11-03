2372e1534073065507da9fd21c71eec648f12265
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-11-03 03:50:38 +0000

    link to lydxn

--- solutions/task048.py
+++ solutions/task048.py
@@ -1,3 +1,4 @@
 p=lambda g:[[hash((*b'`_eL0Q<7MbK9"`@tzn!&a"V%r'[sum(b'	%r'%g)%33:]%g,))%-3&8]]
 # ----------------------------------------------------------------
 # post-comp-diamonds jailctf
+# https://lydxn.github.io/posts/fun-with-magic-hashes-in-python/
