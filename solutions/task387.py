7e1ea223542f4c3a0d9cef1aff1cdb1aa3a1042f
lukegustafson <lukeg3@gmail.com>
2025-11-14 09:00:19 -0500

    387 -3b

--- solutions/task387.py
+++ solutions/task387.py
@@ -1,3 +1,3 @@
-p=lambda g,n=11:-n*g or[[[(i:=((x:=r.pop())+i>0)*-~i)%2*sum(r[:2-i])**4%5*5|x,x or-P**4%5*P*4,sum({*sum(g*(x>9),[])}-{x%15})or x][n//4]for P in[0]+r[:0:-1]]for*r,in zip(*p(g,n-1))if[i:=0]]
+p=lambda g,n=11:-n*g or[[[(i:=((x:=r.pop())+i>0)*-~i)%2*sum(r[:2-i])**4%5*5|x,x or-P**4%5*P*4,[x,sum(sum(g,[]))//2-x%15][x>9]][n//4]for P in[0]+r[:0:-1]]for*r,in zip(*p(g,n-1))if[i:=0]]
 # ----------------------------------------------------------------
 # post-comp-diamonds, base: cgi
