4bef8a2d5ac3a69e6bbd95ca4a240d6c6d8a9c60
Zac-OS <30426887+Zac-OS@users.noreply.github.com>
2025-11-19 07:01:50 +1100

    349 -1b

--- solutions/task349.py
+++ solutions/task349.py
@@ -1,3 +1,3 @@
-e=enumerate;p=lambda g:[[v[j]or any((l:=(m:=W%2*-~m)//2)+1>I-i>~l<J-j<l*3for I,V in e(g)if(m:=1)for J,W in e(V))*3|any(w[:i])for j,w in e(zip(*g))]for i,v in e(g)]
+e=enumerate;p=lambda g:[[v[j]or any((l:=(m:=W%2*-~m)//2)>=I-i>~l<J-j<l*3for I,V in e(g)if(m:=1)for J,W in e(V))*3|any(w[:i])for j,w in e(zip(*g))]for i,v in e(g)]
 # ----------------------------------------------------------------
-# theoreticalsyntaxgolfers
+# post-competition-diamond, base: theoreticalsyntaxgolfers
