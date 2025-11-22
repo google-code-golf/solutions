5007625039fbc7b2c52a9851fbc4dab64e18b53f
Zac-OS <30426887+Zac-OS@users.noreply.github.com>
2025-11-22 16:56:15 +1100

    354 -1b

--- solutions/task354.py
+++ solutions/task354.py
@@ -1,3 +1,7 @@
+p=lambda g:[[c:=[max(j and c,*g[0][j:],key=bool),x][x<5]for j,x in enumerate(r)]for r in g]
+# ----------------------------------------------------------------
+# post-comp-diamonds, base: fuunagent
+
 R=range(10)
 p=lambda g:[[max(g[0][k]*all(r[j:k+1]+r[k:j+1])for k in R)for j in R]for r in g]
 # ----------------------------------------------------------------
